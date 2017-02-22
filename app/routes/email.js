fe0fcd4abc8bfca3c7664c401d698f893a8a8c72
almiche <chamich196@hotmail.com>
2017-02-21 22:17:36 -0500

    Create 3 methods which will be used during the email campaign

---
+++ app/routes/email.js
@@ -0,0 +1,83 @@
+
+module.exports = {
+    forgotPass:function(email){
+    let transporter = nodemailer.createTransport({
+        service: 'outlook',
+        auth: {
+            user: 'chamich196@hotmail.com',
+            pass: 'kngZ1.1@'
+        }
+    });
+
+// setup email data with unicode symbols
+    let mailOptions = {
+        from: '"Famingo Squad Inc." <chamich196@hotmail.com>', // sender address
+        to: email // list of receivers
+        subject: 'SOEN 341 - Password reset', // Subject line
+        text: 'Hello world ?', // plain text body
+        html: '<b>Hello world ?</b>' // html body
+    };
+
+// send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message %s sent: %s', info.messageId, info.response);
+    });
+    },
+
+
+    newInvite : function(email) => {
+        let transporter = nodemailer.createTransport({
+            service: 'outlook',
+            auth: {
+                user: 'chamich196@hotmail.com',
+                pass: 'kngZ1.1@'
+            }
+        });
+
+// setup email data with unicode symbols
+        let mailOptions = {
+            from: '"Famingo Squad Inc." <chamich196@hotmail.com>', // sender address
+            to: email // list of receivers
+            subject: 'SOEN 341 - Password reset', // Subject line
+            text: 'Hello world ?', // plain text body
+            html: '<b>Hello world ?</b>' // html body
+        };
+
+// send mail with defined transport object
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return console.log(error);
+            }
+            console.log('Message %s sent: %s', info.messageId, info.response);
+        });
+    },
+
+   newAccount : function(email) => {
+        let transporter = nodemailer.createTransport({
+            service: 'outlook',
+            auth: {
+                user: 'chamich196@hotmail.com',
+                pass: 'kngZ1.1@'
+            }
+        });
+
+// setup email data with unicode symbols
+        let mailOptions = {
+            from: '"Famingo Squad Inc." <chamich196@hotmail.com>', // sender address
+            to: email // list of receivers
+            subject: 'SOEN 341 - Password reset', // Subject line
+            text: 'Hello world ?', // plain text body
+            html: '<b>Hello world ?</b>' // html body
+        };
+
+// send mail with defined transport object
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return console.log(error);
+            }
+            console.log('Message %s sent: %s', info.messageId, info.response);
+        });
+    }
