2b740250194fb8de1ec158557b3dc8277f38e99b
almiche <chamich196@hotmail.com>
2017-02-21 22:24:36 -0500

    Send email when new user is registered #65

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,7 @@
-const LocalStrategy   = require('passport-local').Strategy;
-const User = require('../app/models/user');
+let LocalStrategy   = require('passport-local').Strategy;
+let User = require('../app/models/user');
+let mailer = require("./app/routes/email");
+
 
 
 module.exports = function(passport) {
@@ -61,7 +63,7 @@ module.exports = function(passport) {
                                 throw err;
                             done(null, newUser);
                         });
-
+                        mailer.newAccount(newUser.email);
                     }
                 });
             });
