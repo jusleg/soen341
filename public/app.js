e3c8451e9b10f9bf890bd60d61bd9d4e2402e0d7
Earthii <xiao_eric@hotmail.com>
2017-03-19 13:25:36 -0400

    added user setting modal, Please to npm install and bower install

--- public/app.js
+++ public/app.js
@@ -5,6 +5,7 @@
     'use strict';
     // Declare app level module which depends on views, and components
     angular.module('app', [
+        'ui.bootstrap',
         'ngRoute',
         'app.header',
         'app.classroom',
@@ -16,7 +17,7 @@
             // $httpProvider.defaults.useXDomain = true;
             // delete $httpProvider.defaults.headers.common['X-Requested-With'];
         }])
-        .controller('appCtrl', function($scope, $rootScope, $http, $routeParams) {
+        .controller('appCtrl', function($scope, $rootScope, $http, $routeParams, $uibModal,$log) {
             var vm = this;
 
             $http.get('/currentUser').then(function(res,err){
@@ -26,6 +27,34 @@
             });
             $rootScope.$watch('currentClassId',function(New){
                 vm.currentClassId = New;
-            })
+            });
+
+            //USER MODAL
+            vm.openUserModal = function (size, parentSelector) {
+                var modalInstance = $uibModal.open({
+                    animation: true,
+                    templateUrl: 'views/userModal.html',
+                    controller: 'userModalInstanceCtrl',
+                    controllerAs: 'vm',
+                    size: size,
+                });
+
+                modalInstance.result.then(function (selectedItem) {
+                    vm.selected = selectedItem;
+                }, function () {
+                    $log.info('Modal dismissed at: ' + new Date());
+                });
+            };
         });
+    angular.module('app').controller('userModalInstanceCtrl', function ($uibModalInstance) {
+        var vm = this;
+
+        vm.ok = function () {
+            $uibModalInstance.close();
+        };
+
+        vm.cancel = function () {
+            $uibModalInstance.dismiss('cancel');
+        };
+    });
 })();
