f716e0c6d01538278ec7344d429e7e8164ac73a1
brendanmcgarry <brendanmcgarry13@gmail.com>
2017-03-20 16:13:19 -0400

    Made page only viewable if user moderates >0 courses (not >-1)

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -30,7 +30,7 @@ module.exports = function(app, passport) {
     });
 
     app.get('/createclass', (req, res) => {
-        if (req.isAuthenticated() && req.user.classMod.length > -1  ) {
+        if (req.isAuthenticated() && req.user.classMod.length > 0) {
             res.sendFile(path.join(__dirname, '../../public/views/create-class.html'));
         } else {
             res.send('You are not authorized to access this page.');
