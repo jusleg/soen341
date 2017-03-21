dcb816c451c2d74e485dd11aac8b38067cf52a4b
brendanmcgarry <brendanmcgarry13@gmail.com>
2017-03-21 18:40:58 -0400

    Can read file contents and manipulate directly in script using memoryStorage instead of diskStorage

--- app/routes/file-upload.js
+++ app/routes/file-upload.js
@@ -1,17 +1,16 @@
 const express = require('express')
 const multer  = require('multer')
-// var storage = multer.memoryStorage()
-// var upload = multer({ storage: storage })
-const upload = multer({ dest: 'uploads/' })
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage, inMemory: true })
 
 module.exports = function(app) {
-	app.post('/createclass', upload.single('studentList')/*, function(req, res, next) {
+	app.post('/createclass', upload.single('studentList'), function(req, res, next) {
 		// If file is smaller than 1 MB
 		if(req.file.size < 1000000) {
-			;
+			console.log(req.file.buffer.toString('utf8'));
 		}
 		else {
 			console.error("Attempted file upload exceeded max size of 1 MB: file was " + (req.file.size/1000000).toFixed(2) + " MB.");
 		}
-	}*/)
+	})
 };
