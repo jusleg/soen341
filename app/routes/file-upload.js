2140b4e82db08a47e3767fe44d0e62ad21b954e1
brendanmcgarry <brendanmcgarry13@gmail.com>
2017-03-22 07:23:47 -0400

    Createclass now creates the class in the db and adds it to existing users list of classes; basic form validation added

--- app/routes/file-upload.js
+++ app/routes/file-upload.js
@@ -1,16 +1,99 @@
+//DB requirements
+const User = require('../models/user.js');
+const classroom = require('../models/Classes.js');
+
+//File upload requirements
 const express = require('express')
 const multer  = require('multer')
-const storage = multer.memoryStorage()
+const storage = multer.memoryStorage() //allows the file to be read into memory instead of saved on disk
 const upload = multer({ storage: storage, inMemory: true })
 
 module.exports = function(app) {
-	app.post('/createclass', upload.single('studentList'), function(req, res, next) {
+	app.post('/createclass', upload.single('studentList'), function(req, res) {
 		// If file is smaller than 1 MB
 		if(req.file.size < 1000000) {
-			console.log(req.file.buffer.toString('utf8'));
-		}
+
+			if(true) {//TODO: if valid class information
+
+				var newClass = new classroom({
+					_id:       req.body.classcode,
+					active:    false,
+					name:      req.body.classname,
+					professor: req.user.id,
+					classroom: req.body.location
+					//TODO: add time
+				});
+
+				newClass.save(function(err, data) {
+					if(err) console.error(err);
+					console.log(data);
+				});
+
+				// Mongoose users to update or create
+				var studentList = req.file.buffer.toString('utf8')
+					.split(/[\r\n]+/)
+					.map((e) => (e.trim())); //get students file as array of lines, trim whitespace
+
+				var mods = req.body.TAs
+					.split(',')
+					.map((e) => (e.trim())); //get each TA'split email and trim each one's whitespace
+				mods.push(req.user.email); //teacher should also be a moderator
+
+				getUsers().then(function(userList) {
+
+					// Add course code to courseMod for users who are to be TAs for this course
+					for(var i in mods) {
+						if(userExists(mods[i], userList)) {
+							var selection = { 'id': mods[i] };
+							var updateQuery = { $addToSet: { classMod: req.body.classcode }};
+							var otions = { safe: true, upsert: true };
+							User.update(selection, updateQuery, options, function(err, data) {
+								console.log(data);
+							})
+						}
+						else {
+							// TODO: create the TA's account with this course in courseMod
+							// TODO: notify the TA via email of their new account
+							console.log('candidate TA ' + mods[i] + ' not found in existing user list');
+						}
+					}
+
+					for(var i in studentList) {
+						let studentInfo = studentList[i].split(','); //[0]==email,[1]==name
+
+						if(userExists(studentInfo[0], userList)) {
+							var selection = {'id': studentInfo[0]};
+							var updateQuery = { $addToSet: { classUser: req.body.classcode }}
+							var options = { safe: true, upsert: true };
+							User.update(selection, updateQuery, options, function(err, data) {
+								console.log(data);
+							});
+						}
+						else {
+							// TODO: create the student (with this class's _id in their classUser[])
+							// TODO: Notify student via email of their new account
+							console.log('student ' + studentInfo[0] + ' not found in existing user list');
+						}
+					}
+
+				});
+			}
+		}	
 		else {
-			console.error("Attempted file upload exceeded max size of 1 MB: file was " + (req.file.size/1000000).toFixed(2) + " MB.");
+			console.error("Attempted file upload exceeded max size of 1 MB: file was "
+				+ (req.file.size/1000000).toFixed(2) + " MB.");
 		}
 	})
+
+	function getUsers() {
+		return User.find({}).select('-pass').select('-__v').exec();
+	}
+
+	function userExists(username, userList) {
+		for(var i in userList) {
+			if(username === userList[i].id)
+				return true; //user found
+		}
+		return false; //user not found
+	}
 };
--- public/javascript/createclass.js
+++ public/javascript/createclass.js
@@ -1,18 +1,62 @@
+//TODO: event-based field validation for user's benefit
+
 function submitCreateClass() {
 
-	// Prepare payload; send file as data
-	var formData = new FormData();
-	var file = $('#studentList').get(0).files[0];
-	formData.append('studentList', file, file.name);
+	var classCode = $('#classcode').val().toUpperCase();
+	var className = $('#classname').val();
+	var hours = $('#hours').val();
+	var location = $('#location').val();
+	var TAs = $('#TAs').val();
 
-	$.ajax({
-		url: '/createclass',
-		type: 'POST',
-		data: formData,
-		processData: false,
-		contentType: false,
-		success: function(response) {
-			console.log(response);
-		}
-	})
+	var validInput = true;
+
+	if(classCode.match(/^[A-Z]{4}[0-9]{3,4}([a-zA-Z0-9]| |-)*$/) === null)
+		validInput = false;
+
+	if(className.match(/^ *$/) !== null)
+		validInput = false;
+
+	//if(hours.match(/^(mon|tue|wed|thu|fri|sat|sun)\/$/)) //TODO: hours validation, e.g. tue/17:45/20:15,thurs/8:45/10:00
+	if(hours.match(/^ *$/) !== null)
+		validInput = false;
+
+	//if(location) //TODO: location validation
+	if(location.match(/^ *$/) !== null)
+		validInput = false;
+	
+	if(TAs.split(',').every(validEmail) === false)
+		validInput = false;
+
+	if(validInput) {
+
+		// Prepare payload; send file as data
+		var formData = new FormData();
+
+		formData.append('classcode', classCode);
+		formData.append('classname', className);
+		formData.append('hours', hours);
+		formData.append('location', location);
+		formData.append('TAs', TAs);
+
+		var file = $('#studentList').get(0).files[0];
+		formData.append('studentList', file, file.name);
+
+		$.ajax({
+			url: '/createclass',
+			type: 'POST',
+			data: formData,
+			processData: false,
+			contentType: false,
+			success: function(response) {
+				console.log(response);
+			}
+		})
+	}
+	else {
+		//TODO: error message
+	}
 };
+
+function validEmail(email) {
+	return email.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/igm) !== null;
+}
