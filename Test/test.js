f279e074613e0b73f299db596f975353ea557751
brendanmcgarry <brendanmcgarry13@gmail.com>
2017-04-12 17:01:19 -0400

    Replaced test.js to be the same as in the develop branch

--- Test/test.js
+++ Test/test.js
@@ -1,22 +1,21 @@
-'use strict';
-
 /**
  * Created by mike on 2/26/17.
  */
 
-var expect = require('chai').expect;
-var assert = require('chai').assert;
-var User = require('../app/models/user');
-var mailer = require('../app/routes/email.js');
-var MongoClient = require('mongodb').MongoClient;
-var url = 'mongodb://chevaldeguerre.xyz:27017/famongo';
-var session = require('supertest-session');
-var myApp = require('./../index.js');
+const expect = require('chai').expect;
+const assert = require('chai').assert;
+const User = require('../app/models/user');
+const mailer = require('../app/routes/email.js');
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://chevaldeguerre.xyz:27017/famongo';
+const session = require('supertest-session');
+const myApp = require('./../index.js');
 var request = require('superagent');
 
+
+
 var api = require('../app/routes/api');
-var chai = require('chai'),
-    chaiHttp = require('chai-http');
+var chai = require('chai'), chaiHttp = require('chai-http');
 var server = require('../index.js');
 var should = chai.should();
 chai.use(chaiHttp);
@@ -24,13 +23,15 @@ chai.use(chaiHttp);
 var superagent = require('superagent');
 var agent = superagent.agent();
 exports.login = function (request, done) {
-    request.post('/login').send(theAccount).end(function (err, res) {
-        if (err) {
-            throw err;
-        }
-        agent.saveCookies(res);
-        done(agent);
-    });
+    request.post('/login')
+        .send(theAccount)
+        .end(function (err, res) {
+            if (err) {
+                throw err;
+            }
+            agent.saveCookies(res);
+            done(agent);
+        });
 };
 
 // // var request = require('supertest')(app); var login = require('/login');
@@ -56,46 +57,59 @@ exports.login = function (request, done) {
 
 describe('RestApi Routes must all be Functional', function () {
     it('it should GET all Users', function () {
-        chai.request(server).get('/api/users').end(function (err, res) {
-            res.should.have.status(200);
-            res.body.should.be.an('array');
-        });
+        chai.request(server)
+            .get('/api/users')
+            .end(function (err, res) {
+                res.should.have.status(200);
+                res.body.should.be.an('array');
+            });
     });
     it('it should get all Messages from the Database', function () {
-        chai.request(server).get('/api/messages/engr371').end(function (err, res) {
-            res.should.have.status(200);
-        });
+        chai.request(server)
+            .get('/api/messages/engr371')
+            .end(function (err, res) {
+                res.should.have.status(200);
+            });
     });
     it('it should post new messages to the Database', function () {
-        chai.request(server).post('/api/message/testing').end(function (err, res) {
-            res.should.have.status(200);
-        });
+        chai.request(server)
+            .post('/api/message/testing')
+            .end(function (err, res) {
+                res.should.have.status(200);
+            });
     });
 
     it('it should receive the currentUser info', function () {
 
-        chai.request(server).post('/login').send({
-            email: 'example@example.com',
-            pass: 'example',
-            id: '134234234'
-        }).end(function (err, res) {
-            res.should.have.status(200);
-            console.log("Done!");
-        });
+        chai.request(server)
+            .post('/login')
+            .send({
+                email: 'example@example.com',
+                pass: 'example',
+                id: '134234234'
+            })
+            .end(function (err, res) {
+                res.should.have.status(200);
+                console.log("Done!");
+            })
 
-        chai.request(server).get('/api/currentUser').end(function (err, res) {
-            res.should.have.status(200);
-        });
-    });
+        chai.request(server)
+            .get('/api/currentUser')
+            .end(function (err, res) {
+                res.should.have.status(200);
+            })
+    })
 });
 
-describe('Sprint 5 testing', function () {
+describe('Sprint 5 testing',function() {
 
     it('Should pass if reverification page is redirected to', function () {
-        chai.request('http://localhost:9001').get('/reverify/steven@hotmail.com').end(function (err, res) {
-            res.should.have.status(302);
-            console.log(err.body);
-        });
+        chai.request('http://localhost:9001')
+            .get('/reverify/steven@hotmail.com')
+            .end(function (err, res) {
+                res.should.have.status(302);
+                console.log(err.body);
+            });
     });
 });
 
@@ -105,9 +119,9 @@ describe('Password tests', function () {
         if (s == null) {
             return null;
         }
-        var tempString = s.split("").reduce(function (a, b) {
-            a = (a << 5) - a + b.charCodeAt(0);
-            return a & a;
+        let tempString = s.split("").reduce(function (a, b) {
+            a = ((a << 5) - a) + b.charCodeAt(0);
+            return a & a
         }, 0);
         return tempString;
     };
@@ -149,6 +163,7 @@ describe('Password tests', function () {
 
         assert.equal(hashedOutput, correctString, '==');
 
+
         done();
     });
 
@@ -163,37 +178,45 @@ describe('Password tests', function () {
     });
 });
 
-describe('Authentication tests', function () {
+
+describe('Authentication tests', function() {
 
     it('Should be able to login successfully with a valid username and password ', function (done) {
-        request.post('/login').send({ email: 'steven1@email.com', password: 'Steven1111' }).end(function (err, res) {
-            done();
-        });
+        request.post('/login')
+            .send({email: 'steven1@email.com', password: 'Steven1111'})
+            .end(function(err,res){
+                done();
+            });
     });
 });
 
-describe('Database fields validatiion', function () {
+describe('Database fields validatiion', function(){
 
-    it('should login', function (done) {
-        request.post('/login').send({ email: 'kevin1@email.com', password: 'Kevin1111' }).end(function (err, res) {
-            User.findOne({ id: 'kevin1@email.com' }, function (err, user) {
-                console.log(user);
-                // expect(user.online).to.be.false;
-                done();
+    it('should login', function(done) {
+        request.post('/login')
+            .send({email: 'kevin1@email.com', password: 'Kevin1111'})
+            .end(function (err, res) {
+                User.findOne({id: 'kevin1@email.com'}, function (err, user) {
+                    console.log(user);
+                    // expect(user.online).to.be.false;
+                    done();
+                });
             });
-        });
     }).timeout(10000);
+
 });
 
-describe('Database fields validatiion', function () {
+describe('Database fields validatiion', function(){
 
-    it('should login', function (done) {
-        request.post('/login').send({ email: 'kevin1@email.com', password: 'Kevin1111' }).end(function (err, res) {
-            User.findOne({ id: 'kevin1@email.com' }, function (err, user) {
-                console.log(user);
-                // expect(user.online).to.be.false;
-                done();
+    it('should login', function(done) {
+        request.post('/login')
+            .send({email: 'kevin1@email.com', password: 'Kevin1111'})
+            .end(function (err, res) {
+                User.findOne({id: 'kevin1@email.com'}, function (err, user) {
+                    console.log(user);
+                    // expect(user.online).to.be.false;
+                    done();
+                });
             });
-        });
     }).timeout(10000);
-});
+    });
